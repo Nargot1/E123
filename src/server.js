a91eb7b439cb55f1faf42214f20790892e4eae07
smierdziel <jaja26960@gmail.com>
2025-12-08 11:24:48 +0100

    init fix

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const app = require("./app")
 
 const { connectDB } = require('./data/connection')
 
+const PORT = 3000;
+
 connectDB().then(() => app.listen(PORT,
     () => {
         console.log("server i running on 3000")
